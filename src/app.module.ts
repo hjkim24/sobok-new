415fe79cd4826d9da6455868127c2ca043ffed38
Kim Hakjae <hjkim4842@gmail.com>
2026-03-10 14:50:05 +0900

    chore: update cloud-init and app module to set NODE_ENV for production and ignore .env file in production

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,10 @@ import { SmsModule } from './sms/sms.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
     ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
